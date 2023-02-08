f26cefd2404037f3ccf2d22bfe8274ca931fb307
Boris Porodnov <porodnov.b@mail.ru>
2023-02-08 15:05:05 +0700

    add file for test for analyze work CI/CD

---
+++ src/test/java/net/porodnov/bank/calculator.java
@@ -0,0 +1,20 @@
+package net.porodnov.bank;
+
+import org.junit.jupiter.api.Test;
+
+class calculator {
+
+    @Test
+    public  void sum() {
+        int valueFirst = 4;
+        int valueSecond = 6;
+        System.out.println(valueFirst + valueSecond + "1 : Result operation");
+    }
+
+    @Test
+    public  void sumSecond() {
+        int valueFirst = 4;
+        int valueSecond = 6;
+        System.out.println(valueFirst + valueSecond + "2 : Result operation");
+    }
+}
